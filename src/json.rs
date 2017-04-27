8856da998b200c12af4952b1aa6b21a3b1db8adb
Oliver Hamlet <oliver.hamlet@gmail.com>
2017-04-28 00:54:51 +0100

    Improve JSON deserialisation
    Deserialise extras field, and deserialise timestamps to 64-bit signed
    integers, instead of strings.
    
    Activity types and extra types are enums, and extra's name field seems
    to be constant, but I've left them as strings for now.

--- src/json.rs
+++ src/json.rs
@@ -1,6 +1,3 @@
-
-use serde_json;
-
 #[derive(Deserialize, PartialEq, Debug)]
 struct GoogleLocationHistory {
     locations: Vec<Location>,
@@ -9,7 +6,8 @@ struct GoogleLocationHistory {
 #[derive(Deserialize, PartialEq, Debug)]
 #[serde(rename_all = "camelCase")]
 struct Location {
-    timestamp_ms: String,
+    #[serde(deserialize_with = "i64_string::deserialize")]
+    timestamp_ms: i64,
     latitude_e7: i64,
     longitude_e7: i64,
     accuracy: u16,
@@ -19,8 +17,10 @@ struct Location {
 #[derive(Deserialize, PartialEq, Debug)]
 #[serde(rename_all = "camelCase")]
 struct TimestampedActivity {
-    timestamp_ms: String,
+    #[serde(deserialize_with = "i64_string::deserialize")]
+    timestamp_ms: i64,
     activities: Vec<Activity>,
+    extras: Option<Vec<Extra>>,
 }
 
 #[derive(Deserialize, PartialEq, Debug)]
@@ -30,15 +30,52 @@ struct Activity {
     confidence: u16,
 }
 
+#[derive(Deserialize, PartialEq, Debug)]
+struct Extra {
+    #[serde(rename = "type")]
+    extra_type: String,
+    name: String,
+    #[serde(rename = "intVal")]
+    int_val: u8,
+}
+
+mod i64_string {
+    use serde::{de, Deserialize, Deserializer};
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<i64, D::Error>
+        where D: Deserializer<'de> {
+        String::deserialize(deserializer)?.parse::<i64>().map_err(de::Error::custom)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    use serde_json;
 
     #[test]
     fn can_deserialize_a_google_location_history_file() {
         let s = r##"
             {
                 "locations" : [ {
+                    "timestampMs" : "1498358433377",
+                    "latitudeE7" : 5207967334,
+                    "longitudeE7" : 11965831,
+                    "accuracy" : 18,
+                    "activitys" : [ {
+                      "timestampMs" : "1498358433377",
+                      "activities" : [ {
+                        "type" : "still",
+                        "confidence" : 100
+                      } ],
+                      "extras" : [ {
+                        "type" : "value",
+                        "name" : "vehicle_personal_confidence",
+                        "intVal" : 100
+                      } ]
+                    } ]
+                }, {
                     "timestampMs" : "1498358433377",
                     "latitudeE7" : 5207967334,
                     "longitudeE7" : 11965831,
@@ -62,19 +99,37 @@ mod tests {
 
         assert_eq!(glh, GoogleLocationHistory {
             locations: vec![Location {
-                timestamp_ms: "1498358433377".to_string(),
+                timestamp_ms: 1498358433377,
                 latitude_e7: 5207967334,
                 longitude_e7: 11965831,
                 accuracy: 18,
                 activitys: Some(vec![TimestampedActivity {
-                    timestamp_ms: "1498358433377".to_string(),
+                    timestamp_ms: 1498358433377,
                     activities: vec![Activity {
                         activity_type: "still".to_string(),
                         confidence: 100,
-                    }]
+                    }],
+                    extras: Some(vec![Extra {
+                        extra_type: "value".to_string(),
+                        name: "vehicle_personal_confidence".to_string(),
+                        int_val: 100,
+                    }]),
                 }]),
             }, Location {
-                timestamp_ms: "1493657963571".to_string(),
+                timestamp_ms: 1498358433377,
+                latitude_e7: 5207967334,
+                longitude_e7: 11965831,
+                accuracy: 18,
+                activitys: Some(vec![TimestampedActivity {
+                    timestamp_ms: 1498358433377,
+                    activities: vec![Activity {
+                        activity_type: "still".to_string(),
+                        confidence: 100,
+                    }],
+                    extras: None,
+                }]),
+            }, Location {
+                timestamp_ms: 1493657963571,
                 latitude_e7: 5205674674,
                 longitude_e7: 11485831,
                 accuracy: 18,

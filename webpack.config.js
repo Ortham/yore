1c5397228bc8b8fd90ae5ad92b250511ef90cb50
Oliver Hamlet <oliver.hamlet@gmail.com>
2018-01-19 17:49:01 +0000

    Add boilerplate for browser-based UI that uses React

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+// Constant with our paths
+const paths = {
+  DIST: path.resolve(__dirname, 'dist'),
+  SRC: path.resolve(__dirname, 'src/gui'),
+  JS: path.resolve(__dirname, 'src/gui/js'),
+};
+
+// Webpack configuration
+module.exports = {
+context: path.join(__dirname),
+  entry: path.join(paths.JS, 'app.js'),
+  output: {
+    path: paths.DIST,
+    filename: 'app.bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(paths.SRC, 'index.html'),
+    }),
+    new CopyWebpackPlugin([
+        { from: 'src/gui/style.css' }
+    ], {
+        copyUnmodified: true
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};

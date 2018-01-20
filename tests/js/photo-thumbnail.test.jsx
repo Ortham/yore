7968d6c1aa3cbf39b31f570e70d67588bc78ec77
Oliver Hamlet <oliver.hamlet@gmail.com>
2018-01-20 14:17:37 +0000

    Add component for selectable photo thumbnails

---
+++ tests/js/photo-thumbnail.test.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import PhotoThumbnail from '../../src/gui/js/photo-thumbnail';
+
+describe('PhotoThumbnail', () => {
+  let mockHandleSelect;
+  let thumbnail;
+
+  beforeEach(() => {
+    mockHandleSelect = jest.fn();
+
+    thumbnail = renderer
+      .create(
+        <PhotoThumbnail
+          key={0}
+          style={{ top: 5 }}
+          isSelected={false}
+          handleSelect={mockHandleSelect}
+          photo={{
+            path: 'path',
+            src: 'source'
+          }}
+        />
+      )
+      .toJSON();
+  });
+
+  test('renders a div with an image and an icon', () => {
+    expect(thumbnail).toMatchSnapshot();
+  });
+
+  test('clicking on a PhotoThumbnail should call its handleSelect callback', () => {
+    thumbnail.props.onClick();
+    expect(mockHandleSelect.mock.calls.length).toBe(1);
+  });
+
+  test('a key-up on a PhotoThumbnail should call its handleSelect callback', () => {
+    thumbnail.props.onKeyUp();
+    expect(mockHandleSelect.mock.calls.length).toBe(1);
+  });
+});

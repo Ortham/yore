d4d9dcbf599f83c7a57748e827ecd145530e18bf
Oliver Hamlet <oliver.hamlet@gmail.com>
2018-01-21 09:22:50 +0000

    Render the Page component, add styling and server requests

--- src/gui/js/app.jsx
+++ src/gui/js/app.jsx
@@ -1,4 +1,94 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Page from './page';
 
-ReactDOM.render(<div />, document.getElementById('root'));
+function getInitialState() {
+  const state = {
+    rootPath: undefined,
+    filterPhotos: false,
+    currentPhoto: undefined,
+    photos: []
+  };
+
+  return fetch('/rootPath')
+    .then(response => response.json())
+    .then(responseBody => {
+      state.rootPath = responseBody.rootPath;
+
+      return fetch('/photos');
+    })
+    .then(response => response.json())
+    .then(responseBody => {
+      state.photos = responseBody.photos.map(photo =>
+        Object.assign(photo, {
+          src: `/thumbnail?path=${encodeURIComponent(
+            photo.path
+          )}&maxWidth=500&maxHeight=500`
+        })
+      );
+
+      return state;
+    });
+}
+
+function getFilteredPhotos(photos) {
+  return fetch('/photos?filter')
+    .then(response => response.json())
+    .then(responseBody =>
+      responseBody.photo_indices.map(index =>
+        Object.assign({}, photos[index], { index })
+      )
+    );
+}
+
+function writeCoordinates(path, coordinates) {
+  const url = `/location?path=${encodeURIComponent(path)}`;
+  const init = {
+    method: 'PUT',
+    body: JSON.stringify(coordinates)
+  };
+
+  return fetch(url, init).then(response => {
+    if (!response.ok) {
+      throw new Error(`Failed to ${init.method} ${url} with body ${init.body}`);
+    }
+  });
+}
+
+function getLocations(startIndex, endIndex) {
+  const url = `/locations?start=${startIndex}&end=${endIndex}`;
+
+  return fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(`Failed to GET ${url}`);
+    })
+    .then(responseBody => responseBody.locations);
+}
+
+function getLocation(path) {
+  const url = `/location?path=${encodeURIComponent(path)}`;
+
+  return fetch(url).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error(`Failed to GET ${url}`);
+  });
+}
+
+getInitialState().then(state => {
+  ReactDOM.render(
+    <Page
+      rootPath={state.rootPath}
+      photos={state.photos}
+      writeCoordinates={writeCoordinates}
+      getLocations={getLocations}
+      getLocation={getLocation}
+      getFilteredPhotos={getFilteredPhotos}
+    />,
+    document.getElementById('root')
+  );
+});
